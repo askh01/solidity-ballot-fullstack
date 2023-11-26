53b8c1c60b539f590db7da84f481fc51060c8280
Asadullah Khalid <Asad-Ullah.Khalid@telekom.de>
2023-11-26 11:41:14 -0800

    Add tokenized ballot smart contract and hardhat config

---
+++ ballot-backend/contracts/TokenizedBallot.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IMyToken {
+    function getPastVotes(address, uint256) external view returns (uint256);
+}
+
+contract TokenizedBallot {
+    struct Proposal {
+        bytes32 name;
+        uint voteCount;
+    }
+
+    IMyToken public tokenContract;
+    Proposal[] public proposals;
+    uint256 public targetBlockNumber;
+    mapping(address => uint256) votingPowerSpent;
+
+    constructor(
+        bytes32[] memory _proposalNames,
+        IMyToken _tokenContract,
+        uint256 _targetBlockNumber
+    ) {
+        tokenContract = IMyToken(_tokenContract);
+        targetBlockNumber = _targetBlockNumber;
+        require(
+            _targetBlockNumber < block.number,
+            'TokenizedBallot: Target block number must be in the past'
+        );
+        for (uint i = 0; i < _proposalNames.length; i++) {
+            proposals.push(Proposal({name: _proposalNames[i], voteCount: 0}));
+        }
+    }
+
+    function vote(uint256 proposal, uint256 amount) external {
+        require(
+            votingPower(msg.sender) >= amount,
+            'TokenizedBallot: trying to vote more than allowed'
+        );
+        proposals[proposal].voteCount += amount;
+        votingPowerSpent[msg.sender] += amount;
+    }
+
+    function votingPower(address account) public view returns (uint256) {
+        return
+            tokenContract.getPastVotes(account, targetBlockNumber) -
+            votingPowerSpent[account];
+    }
+
+    function getRemainingVotingPower(
+        address account
+    ) public view returns (uint256) {
+        return votingPowerSpent[account];
+    }
+
+    function winningProposal() public view returns (uint winningProposal_) {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
